f44691f5dcc773d02d8464b7b1a0c7c2b149ac0e
razonyang <razonyang@gmail.com>
2023-03-23 20:36:37 +0800

    style: hide/show the sticky header when scroll down/up

--- assets/hb/modules/header/js/index.ts
+++ assets/hb/modules/header/js/index.ts
@@ -1,2 +1,28 @@
 import "js/bootstrap/src/dropdown";
 import "js/bootstrap/src/offcanvas";
+
+(() => {
+    document.addEventListener('DOMContentLoaded', () => {
+        const header = document.querySelector('.hb-header') as HTMLElement
+        if (!header) {
+            return;
+        }
+
+        const show = () => {
+            header.style.removeProperty('opacity');
+            header.style.removeProperty('z-index');
+        }
+
+        const hide = () => {
+            header.style.opacity = '0';
+            header.style.zIndex = '0';
+        }
+
+        let h = 0;
+        window.addEventListener('scroll', () => {
+            console.log( document.documentElement.scrollTop, h)
+            document.documentElement.scrollTop > h ? hide() : show();
+            h = document.documentElement.scrollTop;
+        })
+    })
+})()
