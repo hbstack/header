72286c824ebbb7e71be420a361315f5a98ddf3ee
razonyang <razonyang@gmail.com>
2024-06-17 11:00:50 +0800

    fix: detect menus state on detailed pages

--- assets/hb/modules/header/js/index.ts
+++ assets/hb/modules/header/js/index.ts
@@ -9,15 +9,46 @@ import "js/bootstrap/src/offcanvas";
             return;
         }
 
-        const menus = nav.querySelectorAll(`a[href="${window.location.href}"], a[href="${window.location.pathname}"]`)
-        menus.forEach(menu => {
+        const activeMenu = (menu: Element) => {
             menu.classList.add('active')
             menu.setAttribute('aria-current', 'page')
             menu.parentElement?.classList.add('active')
             const parent = menu.closest('.hb-header-menu')
             parent?.classList.add('active')
             parent?.querySelector('.nav-link:first-child')?.classList.add('active')
-        })
+        }
+
+        const isMenuParent = (parent) => {
+            const href = parent.getAttribute('href') ?? ''
+            return window.location.href.indexOf(href) !== -1 || window.location.pathname.indexOf(href) !== -1
+        }
+
+        const menus = nav.querySelectorAll(`a[href="${window.location.href}"], a[href="${window.location.pathname}"]`)
+        if (menus.length > 0) {
+            menus.forEach(menu => {
+                activeMenu(menu)
+            })
+        } else {
+            let foundParent = false
+            const submenus = Array.from(document.querySelectorAll('a.hb-header-submenu'))
+            // find parent menu from second-level menus.
+            for (let menu of submenus) {
+                if (isMenuParent(menu)) {
+                    activeMenu(menu)
+                    foundParent = true
+                    break
+                }
+            }
+            if (!foundParent) {
+                // find parent menu from top-level menus.
+                for (let menu of Array.from(nav.querySelectorAll('a.nav-link'))) {
+                    if (isMenuParent(menu)) {
+                        activeMenu(menu)
+                        break
+                    }
+                }
+            }
+        }
 
         const shadow = () => {
             nav.classList.add('shadow-sm')
